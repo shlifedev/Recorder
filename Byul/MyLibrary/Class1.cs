3bf9c4d6d8886a9c43ab16fbeaca9def9de03c66
Cheese Allergy Hamster <shlifedev@gmail.com>
2021-06-25 04:10:58 +0900

    Installer

---
+++ Byul/MyLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public static class Caller
+    {
+        [DllImport("shlifedev.dll", CallingConvention = CallingConvention.FastCall)]
+        public extern static void Initialize(IntPtr hWnd, float tick);
+
+        [DllImport("shlifedev.dll")]
+        public extern static void InstallHIDDriver();
+
+
+    }
+}
