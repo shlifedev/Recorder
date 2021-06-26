b7f3026a3b8e23103dc20b257500d5b534c4de35
Cheese Allergy Hamster <shlifedev@gmail.com>
2021-06-26 17:12:43 +0900

    recorder

--- Byul/ByulMacro/Core/Components/IORecordController.cs
+++ Byul/ByulMacro/Core/Components/IORecordController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ByulMacro.Core.Attributes;
+using ByulMacro.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,72 @@ namespace ByulMacro.Core.Components
 {
     public static class IORecordController
     {
+        public static List<RecordData> recordDatas = new List<RecordData>();
+
+        public struct RecordData
+        {
+            public bool isMouseEvent;
+            public Hook.HookMouseEvent mouseEvent;
+            public Hook.HookKeyEvent keyEvent;
+            public double eventTime;
+            public RecordData(bool isMouseEvent, Hook.HookMouseEvent mouseEvent, Hook.HookKeyEvent keyEvent, double eventTime)
+            {
+                this.isMouseEvent = isMouseEvent;
+                this.mouseEvent = mouseEvent;
+                this.keyEvent = keyEvent;
+                this.eventTime = eventTime;
+            }
+        }
+
+        private static bool IsStartRecord = false;
+        private static System.DateTime StartTime;
+        private static System.DateTime EndTIme;
         
+
+        public static void Start()
+        {
+            IsStartRecord = true;
+            StartTime = System.DateTime.Now;
+        }
+
+        public static void Stop()
+        {
+            IsStartRecord = false;
+            EndTIme = System.DateTime.Now;
+        }
+
+        public static System.DateTime[] GetStartAndEndTime() { return new DateTime[] { StartTime, EndTIme }; }
+        [Run]
+        public static void RegEvent()
+        {
+            Hook.onKeyboardEvent += OnKeyEvent;
+            Hook.onMouseEvent += OnMouseEvent;
+        }
+
+
+        public static void OnKeyEvent(Hook.HookKeyEvent e)
+        {
+            if (IsStartRecord)
+            {
+                RecordData rd = new RecordData();
+                rd.isMouseEvent = false;
+                rd.keyEvent = e;
+                rd.eventTime = (System.DateTime.Now - StartTime).TotalMilliseconds;
+                recordDatas.Add(rd);
+            }
+          
+        }
+        public static void OnMouseEvent(Hook.HookMouseEvent e)
+        {
+            if (IsStartRecord)
+            {
+                RecordData rd = new RecordData();
+                rd.isMouseEvent = true;
+                rd.mouseEvent = e;
+                rd.eventTime = (System.DateTime.Now - StartTime).TotalMilliseconds;
+                recordDatas.Add(rd);
+            }
+        }
+
     }
 }
--- Byul/ByulMacro/Input/Hook.cs
+++ Byul/ByulMacro/Input/Hook.cs
@@ -14,6 +14,52 @@ namespace ByulMacro.Input
     public static class Hook
     {
         static IInputController io;
+
+        public struct HookMouseEvent
+        {
+            public bool isMoveEvent;
+            /// <summary>
+            /// 0 = down
+            /// 1 = up 
+            /// </summary>
+            public int state;
+            /// <summary>
+            /// 0 = left
+            /// 1 = right
+            /// 2 = middle
+            /// 3 = xButton1
+            /// 4 = xButton2
+            /// </summary>
+            public int mouseButton;
+            /// <summary>
+            /// position
+            /// </summary>
+            public int x, y;
+
+            public HookMouseEvent(bool isMoveEvent, int state, int mouseButton, int x, int y)
+            {
+                this.isMoveEvent = isMoveEvent;
+                this.state = state;
+                this.mouseButton = mouseButton;
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        public struct HookKeyEvent
+        {  
+            public KeyState state;
+            public VirtualKeyCode vkCode; 
+            public HookKeyEvent(KeyState state, VirtualKeyCode vkCode)
+            {
+                this.state = state;
+                this.vkCode = vkCode;
+            }
+        }
+
+        public static System.Action<HookMouseEvent> onMouseEvent;
+        public static System.Action<HookKeyEvent> onKeyboardEvent;
+
         public static Dictionary<(VirtualKeyCode key, KeyState state), System.Action<int, int>> MouseHook = new Dictionary<(VirtualKeyCode key, KeyState state), System.Action<int, int>>();
         public static Dictionary<(VirtualKeyCode key, KeyState state), System.Action> KeyboardHook = new Dictionary<(VirtualKeyCode key, KeyState state), Action>();
         public static Dictionary<(VirtualKeyCode k1, VirtualKeyCode k2), System.Action> KeyComboHook = new Dictionary<(VirtualKeyCode k1, VirtualKeyCode k2), Action>();
@@ -106,7 +152,59 @@ namespace ByulMacro.Input
 
             System.Action<int, int> callback;
             MouseHook.TryGetValue((key, state), out callback);
-            if(callback == null) 
+
+
+
+            //callback hook
+            if(key == VirtualKeyCode.Invalid)
+            {
+                onMouseEvent?.Invoke(new HookMouseEvent()
+                {
+                    isMoveEvent = true,
+                    mouseButton = -1,
+                    state = -1,
+                    x = x,
+                    y = y
+                });
+            }
+            else
+            {
+                int mouseBtn = 0;
+                switch (key)
+                {
+                    case VirtualKeyCode.Lbutton:
+                        mouseBtn = 0;
+                        break;
+
+                    case VirtualKeyCode.Rbutton:
+                        mouseBtn = 1;
+                        break;
+
+                    case VirtualKeyCode.Mbutton:
+                        mouseBtn = 2;
+                        break;
+
+                    case VirtualKeyCode.Xbutton1:
+                        mouseBtn = 3;
+                        break;
+
+                    case VirtualKeyCode.Xbutton2:
+                        mouseBtn = 4;
+                        break;
+                }
+                onMouseEvent?.Invoke(new HookMouseEvent()
+                {
+                    isMoveEvent = false,
+                    mouseButton = mouseBtn,
+                    state = (state == KeyState.Down) ? 0 : (state == KeyState.Up) ? 1 : -1,
+                    x = x,
+                    y = y
+                });
+            }
+      
+
+
+            if (callback == null) 
                 return; 
             else 
                 callback?.Invoke(x,y); 
@@ -120,6 +218,13 @@ namespace ByulMacro.Input
             //Console.WriteLine($"{key}, {state}");
 
             Console.WriteLine($"{key} {state}");
+
+            onKeyboardEvent?.Invoke(new HookKeyEvent()
+            {
+                vkCode = key,
+                state = state
+            });
+
             KeyboardHook.TryGetValue((key, state), out callback);
            
             if (callback != null) 
