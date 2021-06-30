a7517ce7856e1f9b74cd1d8e26e3505eb1635c01
Cheese Allergy Hamster <shlifedev@gmail.com>
2021-06-30 15:47:41 +0900

    using 제거

--- src/AutoHamster/Input/Component/IInputController.cs
+++ src/AutoHamster/Input/Component/IInputController.cs
@@ -1,11 +1,7 @@
 ﻿using LowLevelInput.Hooks;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoHamster.Input.Component
 {
